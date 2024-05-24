69c32732f85f34dc256e5667e0c699b2063fc6f5
Adam Poletek Tanweer <113802807+Poltanek@users.noreply.github.com>
2024-05-25 00:23:31 +0100

    1.0.0

---
+++ ManyBalls.py
@@ -0,0 +1,111 @@
+import pygame
+import numpy as np
+from math import *
+
+WINDOW_SIZE =  800
+ROTATE_SPEED = 0.05
+window = pygame.display.set_mode( (WINDOW_SIZE, WINDOW_SIZE) )
+pygame.display.set_caption("Press A, D, W, S, Q, E to rotate, R to reset, move mouse to change scale")
+clock = pygame.time.Clock()
+
+
+projection_matrix = [[1,0,0],
+                     [0,1,0],
+                     [0,0,0]]
+
+cube_points = [n for n in range(8)]
+cube_points[0] = [[-1], [-1], [1]]
+cube_points[1] = [[1],[-1],[1]]
+cube_points[2] = [[1],[1],[1]]
+cube_points[3] = [[-1],[1],[1]]
+cube_points[4] = [[-1],[-1],[-1]]
+cube_points[5] = [[1],[-1],[-1]]
+cube_points[6] = [[1],[1],[-1]]
+cube_points[7] = [[-1],[1],[-1]]
+
+
+def multiply_m(a, b):
+    result = [[0 for col in range(len(b[0]))] for row in range(len(a))]
+
+    for i in range(len(a)):
+        for j in range(len(b[0])):
+            for k in range(len(b)):
+                result[i][j] += a[i][k] * b[k][j]
+
+    return result
+
+a = [[1, 2, 3], [4, 5, 6]]
+b = [[7, 8], [9, 10], [11, 12]]
+
+result = multiply_m(a, b)
+
+def connect_points(i, j, points):
+    pygame.draw.line(window, (255, 255, 255), (points[i][0], points[i][1]) , (points[j][0], points[j][1]))
+
+# Main Loop
+scale = 100
+angle_x = angle_y = angle_z = 0
+while True:
+    clock.tick(60)
+    window.fill((0,0,0))
+    rotation_x = [[1, 0, 0],
+                    [0, cos(angle_x), -sin(angle_x)],
+                    [0, sin(angle_x), cos(angle_x)]]
+
+    rotation_y = [[cos(angle_y), 0, sin(angle_y)],
+                    [0, 1, 0],
+                    [-sin(angle_y), 0, cos(angle_y)]]
+
+    rotation_z = [[cos(angle_z), -sin(angle_z), 0],
+                    [sin(angle_z), cos(angle_z), 0],
+                    [0, 0, 1]]
+
+    points = [0 for _ in range(len(cube_points))]
+    i = 0
+    for point in cube_points:
+        rotate_x = multiply_m(rotation_x, point)
+        rotate_y = multiply_m(rotation_y, rotate_x)
+        rotate_z = multiply_m(rotation_z, rotate_y)
+        point_2d = multiply_m(projection_matrix, rotate_z)
+    
+        x = (point_2d[0][0] * scale) + WINDOW_SIZE/2
+        y = (point_2d[1][0] * scale) + WINDOW_SIZE/2
+
+        points[i] = (x,y)
+        i += 1
+        pygame.draw.circle(window, (255, 0, 0), (x, y), 5)
+
+    connect_points(0, 1, points)
+    connect_points(0, 3, points)
+    connect_points(0, 4, points)
+    connect_points(1, 2, points)
+    connect_points(1, 5, points)
+    connect_points(2, 6, points)
+    connect_points(2, 3, points)
+    connect_points(3, 7, points)
+    connect_points(4, 5, points)
+    connect_points(4, 7, points)
+    connect_points(6, 5, points)
+    connect_points(6, 7, points)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+        
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_r]:
+            angle_y = angle_x = angle_z = 0
+        if keys[pygame.K_a]:
+            angle_y += ROTATE_SPEED
+        if keys[pygame.K_d]:
+            angle_y -= ROTATE_SPEED      
+        if keys[pygame.K_w]:
+            angle_x += ROTATE_SPEED
+        if keys[pygame.K_s]:
+            angle_x -= ROTATE_SPEED
+        if keys[pygame.K_q]:
+            angle_z -= ROTATE_SPEED
+        if keys[pygame.K_e]:
+            angle_z += ROTATE_SPEED      
+          
+    pygame.display.update()
---
+++ OneBall.py
@@ -0,0 +1,69 @@
+import pygame
+import sys
+import random
+
+# Initialize pygame
+pygame.init()
+
+WIDTH = 600
+HEIGHT = 400
+
+# Set up the display
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Bouncing Ball")
+
+# Ball properties
+ball_colour = (255, 255, 255)
+ball_radius = 20
+ball_x = WIDTH // 2
+ball_y = HEIGHT // 2
+ball_speed_x = 0.2
+ball_speed_y = 0.2
+
+# Main game loop
+game_running = True
+FPS = 60
+
+clock = pygame.time.Clock() 
+
+while game_running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_running = False
+    
+    position = (ball_x, ball_y)
+    acceleration = a = (0, 0.1)
+    
+    time = 1 / FPS
+
+    ball_x += ball_speed_x
+    ball_y += ball_speed_y
+
+    ball_speed_x += acceleration[0] * time
+    ball_speed_y += acceleration[1] * time
+
+    # Collisions with window edges
+    if ball_x - ball_radius < 0 or ball_x + ball_radius > WIDTH:
+        ball_speed_x = -ball_speed_x
+        ball_speed_x *= random.uniform(0.9, 1.1)
+    
+    if ball_y - ball_radius < 0 or ball_y + ball_radius > HEIGHT:
+        ball_speed_y = -ball_speed_y
+        ball_speed_y *= random.uniform(0.9, 1.1)
+    
+    screen.fill((0, 0, 0))
+
+    pygame.draw.circle(screen, ball_colour, position, ball_radius)
+
+    pygame.display.flip()
+
+pygame.quit()
+sys.exit()
+
+
+clock = pygame.time.Clock()
+FPS = 60
+
+while game_running:
+    clock.tick(FPS)
+    # Rest of your code...
